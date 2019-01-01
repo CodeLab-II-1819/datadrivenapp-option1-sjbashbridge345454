dac4811de39ff8892fb13dd85590a14c19a97c92
sjbashbridge345454 <saul.ashbridge17@bathspa.ac.uk>
2019-01-01 08:38:02 +0000

    Added Tweet & Money Counter
    Added options to count the total number of tweets in the data set and the number of tweets mentioning 'money'.

--- gui/src/main.cpp
+++ gui/src/main.cpp
@@ -1,13 +1,69 @@
 #include "ofMain.h"
 #include "ofApp.h"
+#include <iostream>
+#include <fstream>
+#include <string>
 
-//========================================================================
-int main( ){
-	ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
+using namespace std;
 
-	// this kicks off the running of my app
-	// can be OF_WINDOW or OF_FULLSCREEN
-	// pass in width and height too:
-	ofRunApp(new ofApp());
+int main() {
+
+	string line;
+	int count;
+	int tweetCount = 0;
+	ifstream tweetList;
+	tweetList.open("sampleTweets.csv");
+
+	cout << "GUIDE:" << endl;
+	cout << endl;
+	cout << "Count all Tweets in the data set - 1" << endl;
+	cout << "Count Tweets mentioning the word 'Money' - 2" << endl;
+	cout << endl;
+	cout << "Input: ";
+
+	cin >> count;
+
+	if (count == 1) {
+
+		cout << "Option 1 selected. Counting Tweets..." << endl;
+
+		if (tweetList.good()) {
+
+			while (!tweetList.eof()) {
+				getline(tweetList, line);
+				tweetCount++;
+			}
+		}
+	}
+	else if (count == 2) {
+
+		int offset = 0;
+		cout << "Option 2 selected. Counting Tweets..." << endl;
+
+		if (tweetList.good()) {
+
+			while (!tweetList.eof()) {
+				while (getline(tweetList, line)) {
+					if ((offset = line.find("money", 0)) != string::npos) {
+						tweetCount++;
+					}
+					else if ((offset = line.find("Money", 0)) != string::npos) {
+						tweetCount++;
+					}
+				}
+			}
+		}
+
+	}
+
+	cout << endl;
+	cout << "Counting Complete!" << endl;
+	cout << "Tweets Found: ";
+	cout << tweetCount << endl;
+	cout << endl;
+
+	system("Pause");
 
 }
+
+
