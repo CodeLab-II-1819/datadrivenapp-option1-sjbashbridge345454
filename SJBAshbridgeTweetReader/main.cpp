0e79ddaae79c7ec0c2897f1dd11ffb23260ed78a
sjbashbridge345454 <saul.ashbridge17@bathspa.ac.uk>
2019-01-03 10:03:10 +0000

    Split Functions
    Split back into three files (source.cpp, main.cpp and main.h).

--- SJBAshbridgeTweetReader/main.cpp
+++ SJBAshbridgeTweetReader/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include "main.h"
 
 using namespace std;
 
@@ -13,20 +14,7 @@ using namespace std;
 		ifstream tweetList;
 		tweetList.open("sampleTweets.csv");
 
-		cout << "GUIDE:" << endl;
-		cout << endl;
-		cout << "Count all Tweets in the data set - 1" << endl;
-		cout << "Count Tweets mentioning 'Money' - 2" << endl;
-		cout << "Count Tweets about politics - 3" << endl;
-		cout << "Count and display Tweets mentioning 'Paris' - 4" << endl;
-		cout << "Count and display Tweets mentioning 'DreamWorks' - 5" << endl;
-		cout << "Count and display Tweets mentioning 'Uber' - 6" << endl;
-		cout << "Count and display Tweets from April 2016 - 7" << endl;
-		cout << "Count Tweets published at exactly midnight - 8" << endl;
-		cout << "Count Tweets published at exactly midday - 9" << endl;
-		cout << "Count and display Tweets about Japan - 0" << endl;
-		cout << endl;
-		cout << "Input: ";
+		startMenu();
 
 		cin >> count;
 
---
+++ SJBAshbridgeTweetReader/source.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "main.h"
+
+using namespace std;
+
+void startMenu() {
+
+	cout << "GUIDE:" << endl;
+	cout << endl;
+	cout << "Count all Tweets in the data set - 1" << endl;
+	cout << "Count Tweets mentioning 'Money' - 2" << endl;
+	cout << "Count Tweets about politics - 3" << endl;
+	cout << "Count and display Tweets mentioning 'Paris' - 4" << endl;
+	cout << "Count and display Tweets mentioning 'DreamWorks' - 5" << endl;
+	cout << "Count and display Tweets mentioning 'Uber' - 6" << endl;
+	cout << "Count and display Tweets from April 2016 - 7" << endl;
+	cout << "Count Tweets published at exactly midnight - 8" << endl;
+	cout << "Count Tweets published at exactly midday - 9" << endl;
+	cout << "Count and display Tweets about Japan - 0" << endl;
+	cout << endl;
+	cout << "Input: ";
+
+}
