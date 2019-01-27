9f4fb9d3240e1dd6b170f2bd5086c65b7a94cf19
sjbashbridge345454 <44164521+sjbashbridge345454@users.noreply.github.com>
2019-01-27 11:46:13 +0000

    Finished mock GUI
    Completed mock GUI with scalable text. Some issues persist with text scaling.

--- GUI2/src/ofApp.h
+++ GUI2/src/ofApp.h
@@ -22,14 +22,16 @@ public:
 	int width;
 	int height;
 
+	int textHeight;
+
 	//Declares two colours, 'colourA' and 'colourB'
 	ofColor colourA, colourB;
 
 	//Declares various new strings
-	string welcome, welcome2, option;
-	string options[10];
-
+	string welcome, welcome2, option, option2, option3, option4, option5,
+		option6, option7, option8, option9, option10, output;
+	
 	//Declares new fonts
-	ofTrueTypeFont welcomeTXT, optionTXT;
+	ofTrueTypeFont welcomeTXT, optionTXT, outputTXT;
 
 };
